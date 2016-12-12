43c053ecc871880162bdf548c6ffad643d6a5615
agarridogarcia <agarridogarcia@atanasoff30.danielcastelao.org>
2016-12-12 09:00:09 +0100

    Creo metodo calcular dias

--- src/lovivido/Edad.java
+++ src/lovivido/Edad.java
@@ -4,15 +4,23 @@ package lovivido;
 import javax.swing.JOptionPane;
 
 
-public class edad {
+public class Edad {
     private String nombre;
+     int dias;
+     int a=1;
     
     
    public void calcularMeses(){
    int meses;
-   int a=0;
+   
    meses = (a*12);
    JOptionPane.showMessageDialog(null,  "Meses: "  +meses,"Numero de meses vividos de " +nombre,JOptionPane.INFORMATION_MESSAGE);
 }
+   public void calcularDias (){
+       dias = (a*365);
+       JOptionPane.showMessageDialog(null, "Días: " +dias,"Numero de días vividos de "+nombre,JOptionPane.INFORMATION_MESSAGE);
+      
+       
+   }
     
 }
--- src/lovivido/LoVivido.java
+++ src/lovivido/LoVivido.java
@@ -11,28 +11,33 @@ public class LoVivido {
 public static void main(String[] args) {
 String nombre;//cambio nombres de las variables para que se pueda entender mejor
 String edad;
+nombre = JOptionPane.showInputDialog ("Escriba su nombre: ");
+edad = JOptionPane.showInputDialog ("Escriba su edad: ");
+ 
+Edad calc1=new Edad();//llamamos al método calcular meses
+calc1.calcularMeses();
 
 //int meses;
-int dias;
+//int dias;
 int horas;
 int a;
 
-nombre = JOptionPane.showInputDialog ("Escriba su nombre: ");
-edad = JOptionPane.showInputDialog ("Escriba su edad: ");
+//nombre = JOptionPane.showInputDialog ("Escriba su nombre: ");
+//edad = JOptionPane.showInputDialog ("Escriba su edad: ");
  
 a= Integer.parseInt (edad);
  
 //meses = (a*12);
-dias = (a*365);
-horas = (dias*24);
+//dias = (a*365);
+//horas = (dias*24);
  
  //JOptionPane.showMessageDialog(null,  "Meses: "  +meses,"Numero de meses vividos de " +nombre,JOptionPane.INFORMATION_MESSAGE);
 
 
- JOptionPane.showMessageDialog(null, "Días: " +dias,"Numero de días vividos de "+nombre,JOptionPane.INFORMATION_MESSAGE);
+ //JOptionPane.showMessageDialog(null, "Días: " +dias,"Numero de días vividos de "+nombre,JOptionPane.INFORMATION_MESSAGE);
 
 
- JOptionPane.showMessageDialog(null, "Horas: " +horas,"Numero de horas vividos de "+nombre,JOptionPane.INFORMATION_MESSAGE);
+ //JOptionPane.showMessageDialog(null, "Horas: " +horas,"Numero de horas vividos de "+nombre,JOptionPane.INFORMATION_MESSAGE);
 
    System.exit (0);
 
